73574a9f4799a42e379d87a93066ed1af4f8a979
Marissa Berresford <mberresford@nvgo.com>
2018-12-01 14:30:04 -0600

    Initial setup

---
+++ AdventOfCode2018/Day 1/Day 1.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace AdventOfCode2018.Day_1
+{
+    /// <summary>
+    /// Interaction logic for Day_1.xaml
+    /// </summary>
+    public partial class Day_1 : Window
+    {
+        public Day_1()
+        {
+            InitializeComponent();
+        }
+    }
+}
